bb8f0abbcc7973472c260d87044af99d628af7ef
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-09-22 09:40:24 -0300

    Correct grid for 3d world

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -3,23 +3,24 @@ using UnityEngine;
 public class Grid
 {
     private int _width;
-    private int _height;
+    private int _length;
     private float _cellSize;
     private int [,] _gridArray;
     
-    public Grid(int width, int height, int cellSize, GameObject cellPrefab)
+    public Grid(int width, int length, int cellSize, GameObject cellPrefab)
     {
         _width = width;
-        _height = height;
+        _length = length;
         _cellSize = cellSize;
         
-        _gridArray = new int[width, height];
+        _gridArray = new int[width, length];
         
         for(int x = 0; x < _gridArray.GetLength(0); x++)
         {
-            for(int y = 0; y < _gridArray.GetLength(1); y++)
+            for(int z = 0; z < _gridArray.GetLength(1); z++)
             {
-                GameObject.Instantiate(cellPrefab, GetWorldPosition(x, y), Quaternion.identity);
+                // Just for Debugging
+                GameObject.Instantiate(cellPrefab, GetWorldPosition(x, z), Quaternion.identity);
             }
         }
     }
