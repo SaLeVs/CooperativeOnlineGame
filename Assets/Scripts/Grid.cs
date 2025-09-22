66135b8869e67eb9c2d0e327a2e159c55a24e66e
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-09-22 08:57:28 -0300

    Start grid class

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Grid
+{
+    private int _width;
+    private int _height;
+    private int [,] _gridArray;
+    
+    public Grid(int width, int height)
+    {
+        _width = width;
+        _height = height;
+        _gridArray = new int[width, height];
+    }
+   
+}
