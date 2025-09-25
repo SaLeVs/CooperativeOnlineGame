2326318fabad80155ffe09b6cac679d0841acab0
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-09-25 12:10:48 -0300

    Saving files

--- Assets/Scripts/Inputs/InputReader.cs
+++ Assets/Scripts/Inputs/InputReader.cs
@@ -1,10 +1,25 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static PlayerControls;
 
 [CreateAssetMenu(fileName = "InputReader", menuName = "ScriptableObjects/InputReader", order = 1)]
-public class InputReader : ScriptableObject, PlayerControls.IPlayerActions 
+public class InputReader : ScriptableObject, IPlayerActions 
 {
+        
+    private PlayerControls playerControls;
     
+    private void OnEnable()
+    {
+        if (playerControls == null)
+        {
+            playerControls = new PlayerControls();
+            playerControls.Player.SetCallbacks(this);
+        }
+        
+        playerControls.Player.Enable();
+    }
+
     public void OnMove(InputAction.CallbackContext context)
     {
         
@@ -23,5 +38,14 @@ public class InputReader : ScriptableObject, PlayerControls.IPlayerActions
     public void OnJump(InputAction.CallbackContext context)
     {
         
+    }
+
+    private void OnDisable()
+    {
+        if (playerControls != null)
+        {
+            playerControls.Player.Disable();
+            playerControls.Player.SetCallbacks(null);
+        }
     }
 }
