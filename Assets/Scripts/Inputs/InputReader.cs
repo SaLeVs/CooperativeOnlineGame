7fc87022d54f03f3c3e52fcb2d0f7c4ca4e1d6a5
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-09-26 23:25:06 -0300

    Adding events for send inputs for other scripts

--- Assets/Scripts/Inputs/InputReader.cs
+++ Assets/Scripts/Inputs/InputReader.cs
@@ -6,7 +6,11 @@ using static PlayerControls;
 [CreateAssetMenu(fileName = "InputReader", menuName = "ScriptableObjects/InputReader", order = 1)]
 public class InputReader : ScriptableObject, IPlayerActions 
 {
-        
+    public event Action<bool> OnShootEvent;
+    public event Action<Vector2> OnMoveEvent;
+    public event Action<bool> OnJumpEvent;  
+    public event Action<bool> OnDashEvent;
+    
     private PlayerControls playerControls;
     
     private void OnEnable()
@@ -22,22 +26,43 @@ public class InputReader : ScriptableObject, IPlayerActions
 
     public void OnMove(InputAction.CallbackContext context)
     {
-        
+        OnMoveEvent?.Invoke(context.ReadValue<Vector2>());
     }
 
     public void OnShoot(InputAction.CallbackContext context)
     {
-        
+        if (context.started)
+        {
+            OnShootEvent?.Invoke(true);
+        }
+        else if (context.canceled)
+        {
+            OnShootEvent?.Invoke(false);
+        }
     }
 
     public void OnDash(InputAction.CallbackContext context)
     {
-        
+        if(context.started)
+        {
+            OnDashEvent?.Invoke(true);
+        }
+        else if (context.canceled)
+        {
+            OnDashEvent?.Invoke(false);
+        }
     }
 
     public void OnJump(InputAction.CallbackContext context)
     {
-        
+        if (context.started)
+        {
+            OnJumpEvent?.Invoke(true);
+        }
+        else if (context.canceled)
+        {
+            OnJumpEvent?.Invoke(false); 
+        }
     }
 
     private void OnDisable()
