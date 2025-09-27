b8cb11eb16817cad040fd5e584ad8d5e57832a90
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-09-26 23:55:30 -0300

    All debugs working

--- Assets/Scripts/Inputs/TestInputs.cs
+++ Assets/Scripts/Inputs/TestInputs.cs
@@ -5,33 +5,39 @@ public class TestInputs : MonoBehaviour
 {
     [SerializeField] private InputReader inputReader;
 
-    private void Start()
+    private void OnEnable()
     {
         inputReader.OnMoveEvent += InputReader_OnMoveEvent;
         inputReader.OnShootEvent += InputReader_OnShootEvent;
         inputReader.OnDashEvent += InputReader_OnDashEvent;
         inputReader.OnJumpEvent += InputReader_OnJumpEvent;
-        
-        
     }
 
     private void InputReader_OnMoveEvent(Vector2 movement)
     {
-        
+        Debug.Log($"Input - Move: {movement}");
     }
     
     private void InputReader_OnShootEvent(bool isShooting)
     {
-        
+        Debug.Log($"Input - Shooting: {isShooting}");
     }
     
     private void InputReader_OnDashEvent(bool isDashing)
     {
-        
+        Debug.Log($"Input - Dashing: {isDashing}");
     }
     
     private void InputReader_OnJumpEvent(bool isJumping)
     {
-        
+        Debug.Log($"Input - Jumping: {isJumping}");
+    }
+
+    private void OnDisable()
+    {
+        inputReader.OnMoveEvent -= InputReader_OnMoveEvent;
+        inputReader.OnShootEvent -= InputReader_OnShootEvent;
+        inputReader.OnDashEvent -= InputReader_OnDashEvent;
+        inputReader.OnJumpEvent -= InputReader_OnJumpEvent;
     }
 }
